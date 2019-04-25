e045a4b38f988a982a9605b3ca482422b7694ef5
Sergey Tolchev <stolchev@baltbet.ru>
2019-04-25 12:37:24 +0300

    Рефакторинг.

--- WpfObjectView/ViewModels/ObjectDetailViewModel.cs
+++ WpfObjectView/ViewModels/ObjectDetailViewModel.cs
@@ -1,28 +1,50 @@
-﻿using System;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Windows.Input;
+using WpfObjectView.Behaviors;
 
 namespace WpfObjectView.ViewModels
 {
-    class ObjectDetailViewModel : ConfirmationViewModelBase
+    class ObjectDetailViewModel : BindableBase, IDialogHostViewModel
     {
-        public ObjectDetailViewModel(object item, Action saveAction)
+        public ObjectDetailViewModel(object item, Action action)
         {
             Item = item;
-            SaveAction = saveAction;
+            Action = action;
+
+            OkCommand = new DelegateCommand(OkExecute);
+            CancelCommand = new DelegateCommand(CancelExecute);
         }
 
-        public object Item { get; }
+        public ICommand OkCommand { get; }
+        public ICommand CancelCommand { get; }
 
-        public Action SaveAction { get; }
-
-#warning Пока коммуникация идет через свойство, придумать более правильный способ.
-        public bool UpdateFlag { get; set; }
-
-        protected override void OkExecute()
+#warning Сделать одну команду с параметром.
+        protected virtual void OkExecute()
         {
             UpdateFlag = true;
             RaisePropertyChanged(nameof(UpdateFlag));
-            SaveAction();
-            base.OkExecute();
+            Action();
+            DialogHost.Close();
         }
+
+        protected virtual void CancelExecute()
+        {
+            DialogHost.Close();
+        }
+
+        #region IDialogHostViewModel
+
+        public IDialogHost DialogHost { get; set; }
+
+        #endregion
+
+        public object Item { get; }
+
+        public Action Action { get; }
+
+#warning Пока коммуникация идет через свойство, придумать более правильный способ.
+        public bool UpdateFlag { get; set; }
     }
 }
